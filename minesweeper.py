fc397a204b0d78498c2c23e34bb1b72ef279d2e7
MykalB081 <78550307+MykalB081@users.noreply.github.com>
2021-02-27 10:48:19 -0500

    Getting started

---
+++ minesweeper.py
@@ -0,0 +1,19 @@
+import tkinter as tk
+
+if __name__ == '__main__':
+    #window = tk.Tk()
+    #window.title("Minesweeper")
+    rows = 5
+    columns = 5
+    arr = [[0] * columns] * rows
+    print(arr)
+
+    i = 10
+    x = 0
+    while x < i:
+        print(x, i)
+        x += 1
+
+    #square = tk.Button()
+    #square.grid(row=0, column=0)
+    #window.mainloop()
